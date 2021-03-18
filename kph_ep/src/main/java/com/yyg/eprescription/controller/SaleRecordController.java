7769808dcfc2fe8021707d128efe7ac8eb38bbf9
imoldxu <9794208@qq.com>
2021-03-18 11:41:24 +0800

    去掉无用的引用

--- kph_ep/src/main/java/com/yyg/eprescription/controller/SaleRecordController.java
+++ kph_ep/src/main/java/com/yyg/eprescription/controller/SaleRecordController.java
@@ -17,7 +17,6 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
